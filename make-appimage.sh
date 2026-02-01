1d4cd822fd9d33a3d6388b91e441029c5c89553a
Link4Electronics <link4electronics@gmail.com>
2026-02-01 10:43:27 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/classicube-git/CCicon.png
 export DESKTOP=/usr/share/applications/classicube.desktop
 export DEPLOY_OPENGL=1
-export ANYLINUX_LIB=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/classicube
