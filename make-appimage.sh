ea7190421b273ace83fb05d9ee6e2e039ddac92d
Link4Electronics <link4electronics@gmail.com>
2026-02-04 08:38:49 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/classicube/CCicon.png
+export ICON=/usr/share/pixmaps/CCicon.png
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
